8ea52507059b476cd28593605c046f70269f4f63
alexluczi <luczialex@gmail.com>
2024-01-02 13:40:27 +0100

    MySql Connection

--- KKKonyvtar/Program.cs
+++ KKKonyvtar/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace KKKonyvtar;
+﻿using MySql.Data.MySqlClient;
+
+namespace KKKonyvtar;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string connectionString = "Server=localhost;Database=kkkonyvtar;User=root;Password=root;";
+
+        using MySqlConnection connection = new MySqlConnection(connectionString);
+        connection.Open();
+
+        Console.WriteLine("Connected to MySQL!");
+
+        // Perform database operations here
+
+        connection.Close();
     }
 }
 
